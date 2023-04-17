32a208f88c8b43c8e91fd76e96dd934c27186f63
bkllm <ayoub.bakalem@student.odisee.be>
2023-04-17 13:49:39 +0200

    config pages

---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <RulesPage />
+</template>
+  
+<script>
+import RulesPage from '../components/Settings/MainPage.vue'
+export default {
+  components: {
+    RulesPage,
+  }
+}
+</script>
+  
+<style></style>
