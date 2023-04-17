01daa56a3ee0abfd599ecc0224afb63d2af52e3c
NerfedChovy <ayoub.afkir@student.odisee.be>
2023-04-17 15:05:41 +0200

    Multiplayer page structure

--- src/components/Game/MainPage.vue
+++ src/components/Game/MainPage.vue
@@ -6,7 +6,235 @@
             <a href="#" class="button" id="infobox__end">End game</a>
         </div>
         <div class="main__decks">
-            <div class="decks__deck">
+            <div class="decks__deck decks_deck1">
+                <p class="deck__player">player name</p>
+                <div class="deck__rack">
+                    <div class="rack__row">
+                        <div class="rack__col rack__11">
+                            11
+                        </div>
+                        <div class="rack__col rack__12">
+                            12
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__21">
+                            21
+                        </div>
+                        <div class="rack__col rack__22">
+                            23
+                        </div>
+                        <div class="rack__col rack__23">
+                            23
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__31">
+                            31
+                        </div>
+                        <div class="rack__col rack__32">
+                            32
+                        </div>
+                        <div class="rack__col rack__33">
+                            33
+                        </div>
+                        <div class="rack__col rack__34">
+                            34
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__41">
+                            41
+                        </div>
+                        <div class="rack__col rack__42">
+                            42
+                        </div>
+                        <div class="rack__col rack__43">
+                            43
+                        </div>
+                        <div class="rack__col rack__44">
+                            44
+                        </div>
+                        <div class="rack__col rack__45">
+                            45
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__51">
+                            51
+                        </div>
+                        <div class="rack__col rack__52">
+                            52
+                        </div>
+                        <div class="rack__col rack__53">
+                            53
+                        </div>
+                        <div class="rack__col rack__54">
+                            54
+                        </div>
+                        <div class="rack__col rack__55">
+                            55
+                        </div>
+                        <div class="rack__col rack__56">
+                            56
+                        </div>
+                    </div>
+                </div>
+                <p class="deck__stack">R A C K</p>
+            </div>
+            <div class="decks__deck decks_deck2">
+                <p class="deck__player">player name</p>
+                <div class="deck__rack">
+                    <div class="rack__row">
+                        <div class="rack__col rack__11">
+                            11
+                        </div>
+                        <div class="rack__col rack__12">
+                            12
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__21">
+                            21
+                        </div>
+                        <div class="rack__col rack__22">
+                            23
+                        </div>
+                        <div class="rack__col rack__23">
+                            23
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__31">
+                            31
+                        </div>
+                        <div class="rack__col rack__32">
+                            32
+                        </div>
+                        <div class="rack__col rack__33">
+                            33
+                        </div>
+                        <div class="rack__col rack__34">
+                            34
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__41">
+                            41
+                        </div>
+                        <div class="rack__col rack__42">
+                            42
+                        </div>
+                        <div class="rack__col rack__43">
+                            43
+                        </div>
+                        <div class="rack__col rack__44">
+                            44
+                        </div>
+                        <div class="rack__col rack__45">
+                            45
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__51">
+                            51
+                        </div>
+                        <div class="rack__col rack__52">
+                            52
+                        </div>
+                        <div class="rack__col rack__53">
+                            53
+                        </div>
+                        <div class="rack__col rack__54">
+                            54
+                        </div>
+                        <div class="rack__col rack__55">
+                            55
+                        </div>
+                        <div class="rack__col rack__56">
+                            56
+                        </div>
+                    </div>
+                </div>
+                <p class="deck__stack">R A C K</p>
+            </div>
+            <div class="decks__deck decks_deck3">
+                <p class="deck__player">player name</p>
+                <div class="deck__rack">
+                    <div class="rack__row">
+                        <div class="rack__col rack__11">
+                            11
+                        </div>
+                        <div class="rack__col rack__12">
+                            12
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__21">
+                            21
+                        </div>
+                        <div class="rack__col rack__22">
+                            23
+                        </div>
+                        <div class="rack__col rack__23">
+                            23
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__31">
+                            31
+                        </div>
+                        <div class="rack__col rack__32">
+                            32
+                        </div>
+                        <div class="rack__col rack__33">
+                            33
+                        </div>
+                        <div class="rack__col rack__34">
+                            34
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__41">
+                            41
+                        </div>
+                        <div class="rack__col rack__42">
+                            42
+                        </div>
+                        <div class="rack__col rack__43">
+                            43
+                        </div>
+                        <div class="rack__col rack__44">
+                            44
+                        </div>
+                        <div class="rack__col rack__45">
+                            45
+                        </div>
+                    </div>
+                    <div class="rack__row">
+                        <div class="rack__col rack__51">
+                            51
+                        </div>
+                        <div class="rack__col rack__52">
+                            52
+                        </div>
+                        <div class="rack__col rack__53">
+                            53
+                        </div>
+                        <div class="rack__col rack__54">
+                            54
+                        </div>
+                        <div class="rack__col rack__55">
+                            55
+                        </div>
+                        <div class="rack__col rack__56">
+                            56
+                        </div>
+                    </div>
+                </div>
+                <p class="deck__stack">R A C K</p>
+            </div>
+            <div class="decks__deck decks_deck4">
                 <p class="deck__player">player name</p>
                 <div class="deck__rack">
                     <div class="rack__row">
@@ -90,23 +318,27 @@
 
 </script>
 
-<style>
+<style scoped>
 .main__wrapper {
     margin: 15px;
 }
 
 .button {
-    display: inline-block;
-    text-decoration: none;
-    color: #000;
-    padding: 10px;
     background-color: #888;
     border: #888 solid 1px;
+    color: #000;
+    display: inline-block;
+    padding: 10px;
+    text-decoration: none;
 }
 
 /* Deck */
-.decks__deck p {
-    text-align: center;
+.main__decks {
+    display: flex;
+}
+
+.decks__deck {
+    width: 25%;
 }
 
 .deck__rack {
@@ -115,9 +347,9 @@
 
 .rack__col {
     background-color: #B1AFFF;
-    padding: 10px;
     border: #000 solid 1px;
     display: inline-block;
+    padding: 10px;
 }
 
 .rack__row {
@@ -128,9 +360,10 @@
 /* Infobox */
 .main__infobox {
     background-color: #C8FFD4;
-    text-align: center;
     border: #000 solid 1px;
+    margin-bottom: 15px;
     padding: 15px;
+    text-align: center;
 }
 
 .main__infobox p {
--- src/components/Home/MainPage.vue
+++ src/components/Home/MainPage.vue
@@ -5,5 +5,39 @@
 <script>
 
 </script>
+<<<<<<< Updated upstream
   
 <style></style>
+=======
+
+<style scoped>
+body {
+  background-color: #B9E9FC;
+}
+
+h1 {
+  text-align: center;
+  font-size: 90px;
+  font-family: 'Segoe UI', Tahoma, Verdana, sans-serif;
+  color: #B2B0FF;
+}
+
+#buttons {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.button {
+  margin-bottom: 10px;
+  padding: 10px;
+  font-size: 30px;
+  background-color: #C9FFD5;
+  border-radius: 5px;
+}
+
+.button:hover{
+  background-color: #FDFDBE;
+}
+</style>
+>>>>>>> Stashed changes
