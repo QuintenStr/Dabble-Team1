c33f618e836d33a37b7ad5661b35e371dae1fab8
bkllm <ayoub.bakalem@student.odisee.be>
2023-04-17 14:29:11 +0200

    html/body update

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,25 @@
     <router-link to="/game">Game</router-link>
     <router-link to="/settings">Rules</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+}
+
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
