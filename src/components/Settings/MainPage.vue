469f5a24efbacfc913f21832920b6bc7de2b31e1
ayoubpaila25 <114088202+ayoubpaila25@users.noreply.github.com>
2023-04-17 15:34:46 +0200

    rules

--- src/components/Settings/MainPage.vue
+++ src/components/Settings/MainPage.vue
@@ -1,9 +1,88 @@
 <template>
-    <h1>Rules page</h1>
+    <div id="rules">
+    <h1>Dabble</h1>
+    <h2>The Game Rules</h2>
+
+
+    
+        Each player's goal is to create one 2 letter word, one 3 letter word, one 4 letter word,
+        one 5 letter word and one 6 letter word from 20 selected tiles before the timer runs out.Each player picks 20
+        tiles from the bag without looking at them.As you pick your tiles, place them face down in front of you.Once all
+        the players have selected their tiles, turn the timer over to begin the first round, which lasts until the timer
+        runs out.All players turn their tiles face up and begin to create their 5 words.
+
+        <h3>WHAT TO DO WHEN NO ONE WINS THE ROUND:</h3>
+        If no one is able to create 5 words before the timer runs out, players can choose to
+        exchange any or all of their tiles. Players should put the tiles they chose to exchange
+        into the bag and pick out the same number of tiles as they discarded.Once the new tiles have been selected, the
+        timer is turned over and the next round begins. This method of playing continues until there is a winner.
+
+        <h3>WHAT TO DO WHEN ONE PLAYER WINS THE ROUND:</h3>
+        If one player created 5 words without exchanging tiles, he/she wins the round and gets
+        the total points of the other player's unused tiles (letters not used in a word on their
+        rack) as his/her score for this round. The other player(s) get 0 points for this round.
+
+        <h3>WHAT TO DO WHEN MORE THAN ONE PLAYER WINS THE ROUND:</h3>
+        If more than one player has created 5 words without exchanging tiles, then each
+        winning player gets the total points of the other player's unused tiles. The other
+        player(s) get 0 points for this round.
+
+        <h3>WHAT TO DO WHEN ALL PLAYERS WIN THE ROUND:</h3>
+        If all the players created 5 words without exchanging tiles within the allotted time, then the player with the
+        highest score for any one word on his or her rack is considered the winner of this round and gets the amount of
+        points of that one winning word. The other
+        players get 0 points for this round. If there's still a tie, and all players have the same
+        score for their highest-scoring word, each player receives their points.
+
+        <h3>CHALLENGING A WORD:</h3>
+        When the 5 words are presented to finalize a round, any player can challenge a word,
+        either as being misspelled, or not a word at all. Refer to your personal dictionary to
+        settle challenges. If the challenger is correct and the word is indeed incorrect, the presenter of the word deducts
+        the points from his/her score.
+
+        <h3>SPECIALTY TILES:</h3>
+        Blank tiles have no point value and can be used as any letter, but NOT as an
+        apostrophe.There are 2 apostrophe tiles per game and they have NO point value when used in a word, but count as a
+        tile.Each player including the winner of the round gets double points for a word with an apostrophe. When an
+        apostrophe tile is NOT used in a word by the end of a round, the winner gets 5 points for it.
+
+        <h3>ADVANCED PLAY - SUBJECT WORDS:</h3>
+        At the beginning of each round, choose a “Subject Category” — see below for
+        examples. Players should try to spell subject words whenever possible during that
+        round.
+        — Clothing: hat, scarf, shoe, coat, blouse, jacket, sock, etc.
+        — Foods: cereal, milk, bread, toast, egg, bacon, meat, butter, etc.
+        — House Objects: carpet, lamp, couch, chair, bed, table, bulb, pot, etc.
+        At the end of each round, each player including the winner of the round gets the point value for each subject word
+        used on his/her rack in addition to points earned through regular Dabble play.
+
+        <h3>SPECIAL NOTE:</h3>
+        All words labeled as a part of speech, including those listed of foreign origin, archaic,
+        obsolete, colloquial, and slang are permitted in DABBLE, with the exception of
+        abbreviations, prefixes, suffixes standing alone, and proper nouns. When playing in
+        English, remove the 6 tiles labeled rr, rr, ll, ll, ñ, ñ.
+
+        <h3>GAME CONTENTS:</h3>
+        142 tiles (plus 6 Spanish tiles); 4 five-tier stadium style racks; Sand timer; Tile bag;
+        Rules (see other side for Spanish rules).
+    </div>
 </template>
   
 <script>
 
 </script>
   
-<style></style>
+<style>
+#rules {
+    background-color: #B8E8FC;
+    color:black;
+    text-align: left;
+    background: rgb(0,194,255);
+background: linear-gradient(90deg, rgba(0,194,255,1) 100%, rgba(0,194,255,1) 100%);
+    padding-left: 3%;
+}
+h1 {
+    text-align: center;
+}
+
+</style>
