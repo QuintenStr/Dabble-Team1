934316079607196918209d6be6ad1dbc39a347b4
halloikbenleerling <digay.kengoum@student.odisee.be>
2023-04-19 17:03:21 +0200

    Settings page

--- src/components/Settings/MainPage.vue
+++ src/components/Settings/MainPage.vue
@@ -6,9 +6,9 @@
                 src="https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/hIFJ4757SD1qbJ9veZoewTr0ihjJjpkMAOJZr1G6.mp3?download=1&name=Eggy%20Toast%20-%20Lose%20your%20head.mp3.mp3"
                 loop ref="audio"></audio>
             <div id="buttons">
-                <router-link class="button" to="/">Home</router-link>
                 <button class="button1" v-on:click="changeBackground">Change background</button>
                 <button class="button1">EN/NL</button>
+                <button class="button1" v-on:click="resetBackground">Reset Color</button>
             </div>
         </div>
     </div>
@@ -22,8 +22,6 @@ export default {
     },
     methods: {
         changeBackground() {
-            // const colors = ["redBackground,yellowBackground,greenBackground"];
-            // let colorIndex = 0;
             const bgcolor = `rgb(${+Math.random() * 255}, ${+Math.random() * 255}, ${+Math.random() * 255})`;
             const header = document.getElementById("BckgColor");
             const body = document.querySelector("body");
@@ -31,23 +29,20 @@ export default {
             header.style.backgroundColor = bgcolor;
             console.log('button');
             console.log(body);
-            // background.classList.remove(colors[colorIndex]);
-            // colorIndex++;
-            // if (colorIndex >= colors.length) {
-            // colorIndex = 0;
-        }
-        // background.classList.add(colors[colorIndex]);
+        },
+        resetBackground() {
+            const generalColor = 'rgb(185, 233, 252)';
+            const body = document.querySelector("body");
+            const header = document.getElementById("BckgColor");
+            body.style.backgroundColor = generalColor;
+            header.style.backgroundColor = generalColor;
+        },
     }
 }
 
 </script>
   
 <style scoped>
-.button {
-    position: fixed;
-    bottom: 0;
-    right: 20px;
-}
 
 #volume-slider-container {
     padding: 20px;
@@ -64,12 +59,15 @@ export default {
     text-decoration: none;
     color: black;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    margin-top: 30px;
     cursor: pointer;
 }
 
 .button1:hover {
     background-color: #FDFDBE;
 }
+h1 {
+    margin-bottom: 50px;
+}
+
 </style>
   
