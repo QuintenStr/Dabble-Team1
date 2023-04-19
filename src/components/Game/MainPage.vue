4ee3fb6a547aca23c829489026ed42741f2d6316
halloikbenleerling <digay.kengoum@student.odisee.be>
2023-04-19 10:28:00 +0200

    Show End page

--- src/components/Game/MainPage.vue
+++ src/components/Game/MainPage.vue
@@ -1,326 +1,91 @@
 <template>
   <h1>Game page</h1>
-  <div class="main__wrapper" v-if="!isGameEnded">
+  <div class="main__wrapper" v-if="!hasGameEnded">
     <div class="main__infobox">
       <p class="infobox__timer">Time remaining: {{ minutes }}:{{ seconds.toString().padStart(2, '0') }}</p>
-      <button class="button" v-on:click="isGameEnded = true">END GAME</button>
+      <button class="button" v-on:click="hasGameEnded = true">END GAME</button>
     </div>
-    <div class="main__decks">
-      <div class="decks__deck decks_deck1">
-        <p class="deck__player">player name</p>
-        <div class="deck__rack">
-          <div class="rack__row drop" ref="drop" @dragover.prevent @drop="dropChar">
-            <div class="rack__col rack__11">
-              11
-            </div>
-            <div class="rack__col rack__12">
-              12
-            </div>
+    <div class="main__deck">
+      <p class="deck__player">player name</p>
+      <div class="deck__rack">
+        <div class="rack__row">
+          <div class="rack__col rack__11" ref="drop" @dragover.prevent @drop="dropChar">
+            __
           </div>
-          <div class="rack__row">
-            <div class="rack__col rack__21">
-              21
-            </div>
-            <div class="rack__col rack__22">
-              23
-            </div>
-            <div class="rack__col rack__23">
-              23
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__31">
-              31
-            </div>
-            <div class="rack__col rack__32">
-              32
-            </div>
-            <div class="rack__col rack__33">
-              33
-            </div>
-            <div class="rack__col rack__34">
-              34
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__41">
-              41
-            </div>
-            <div class="rack__col rack__42">
-              42
-            </div>
-            <div class="rack__col rack__43">
-              43
-            </div>
-            <div class="rack__col rack__44">
-              44
-            </div>
-            <div class="rack__col rack__45">
-              45
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__51">
-              51
-            </div>
-            <div class="rack__col rack__52">
-              52
-            </div>
-            <div class="rack__col rack__53">
-              53
-            </div>
-            <div class="rack__col rack__54">
-              54
-            </div>
-            <div class="rack__col rack__55">
-              55
-            </div>
-            <div class="rack__col rack__56">
-              56
-            </div>
+          <div class="rack__col rack__12" ref="drop" @dragover.prevent @drop="dropChar">
+            __
           </div>
         </div>
-        <div class="deck__stack">
-          <div v-for="char in generateChars()" v-html="`${char.value}:${char.score}`" :id="`player1_${char.id}`" data-player="player1"
-            class="stack__char" :key="`player1_${char.id}`" :ref="`player1_${char.id}`" draggable="true"
-            @dragstart="dragChar"> </div>
-          <draggable tag="div">
-
-          </draggable>
+        <div class="rack__row">
+          <div class="rack__col rack__21" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__22" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__23" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+        </div>
+        <div class="rack__row">
+          <div class="rack__col rack__31" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__32" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__33" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__34" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+        </div>
+        <div class="rack__row">
+          <div class="rack__col rack__41" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__42" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__43" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__44" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__45" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+        </div>
+        <div class="rack__row">
+          <div class="rack__col rack__51" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__52" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__53" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__54" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__55" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
+          <div class="rack__col rack__56" ref="drop" @dragover.prevent @drop="dropChar">
+            __
+          </div>
         </div>
       </div>
-      <div class="decks__deck decks_deck2">
-        <p class="deck__player">player name</p>
-        <div class="deck__rack">
-          <div class="rack__row">
-            <div class="rack__col rack__11">
-              11
-            </div>
-            <div class="rack__col rack__12">
-              12
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__21">
-              21
-            </div>
-            <div class="rack__col rack__22">
-              23
-            </div>
-            <div class="rack__col rack__23">
-              23
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__31">
-              31
-            </div>
-            <div class="rack__col rack__32">
-              32
-            </div>
-            <div class="rack__col rack__33">
-              33
-            </div>
-            <div class="rack__col rack__34">
-              34
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__41">
-              41
-            </div>
-            <div class="rack__col rack__42">
-              42
-            </div>
-            <div class="rack__col rack__43">
-              43
-            </div>
-            <div class="rack__col rack__44">
-              44
-            </div>
-            <div class="rack__col rack__45">
-              45
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__51">
-              51
-            </div>
-            <div class="rack__col rack__52">
-              52
-            </div>
-            <div class="rack__col rack__53">
-              53
-            </div>
-            <div class="rack__col rack__54">
-              54
-            </div>
-            <div class="rack__col rack__55">
-              55
-            </div>
-            <div class="rack__col rack__56">
-              56
-            </div>
-          </div>
-        </div>
-        <p class="deck__stack">R A C K</p>
-      </div>
-      <div class="decks__deck decks_deck3">
-        <p class="deck__player">player name</p>
-        <div class="deck__rack">
-          <div class="rack__row">
-            <div class="rack__col rack__11">
-              11
-            </div>
-            <div class="rack__col rack__12">
-              12
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__21">
-              21
-            </div>
-            <div class="rack__col rack__22">
-              23
-            </div>
-            <div class="rack__col rack__23">
-              23
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__31">
-              31
-            </div>
-            <div class="rack__col rack__32">
-              32
-            </div>
-            <div class="rack__col rack__33">
-              33
-            </div>
-            <div class="rack__col rack__34">
-              34
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__41">
-              41
-            </div>
-            <div class="rack__col rack__42">
-              42
-            </div>
-            <div class="rack__col rack__43">
-              43
-            </div>
-            <div class="rack__col rack__44">
-              44
-            </div>
-            <div class="rack__col rack__45">
-              45
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__51">
-              51
-            </div>
-            <div class="rack__col rack__52">
-              52
-            </div>
-            <div class="rack__col rack__53">
-              53
-            </div>
-            <div class="rack__col rack__54">
-              54
-            </div>
-            <div class="rack__col rack__55">
-              55
-            </div>
-            <div class="rack__col rack__56">
-              56
-            </div>
-          </div>
-        </div>
-        <p class="deck__stack">R A C K</p>
-      </div>
-      <div class="decks__deck decks_deck4">
-        <p class="deck__player">player name</p>
-        <div class="deck__rack">
-          <div class="rack__row">
-            <div class="rack__col rack__11">
-              11
-            </div>
-            <div class="rack__col rack__12">
-              12
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__21">
-              21
-            </div>
-            <div class="rack__col rack__22">
-              23
-            </div>
-            <div class="rack__col rack__23">
-              23
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__31">
-              31
-            </div>
-            <div class="rack__col rack__32">
-              32
-            </div>
-            <div class="rack__col rack__33">
-              33
-            </div>
-            <div class="rack__col rack__34">
-              34
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__41">
-              41
-            </div>
-            <div class="rack__col rack__42">
-              42
-            </div>
-            <div class="rack__col rack__43">
-              43
-            </div>
-            <div class="rack__col rack__44">
-              44
-            </div>
-            <div class="rack__col rack__45">
-              45
-            </div>
-          </div>
-          <div class="rack__row">
-            <div class="rack__col rack__51">
-              51
-            </div>
-            <div class="rack__col rack__52">
-              52
-            </div>
-            <div class="rack__col rack__53">
-              53
-            </div>
-            <div class="rack__col rack__54">
-              54
-            </div>
-            <div class="rack__col rack__55">
-              55
-            </div>
-            <div class="rack__col rack__56">
-              56
-            </div>
-          </div>
-        </div>
-        <p class="deck__stack">R A C K</p>
+      <div class="deck__stack">
+        <div v-for="char in stack" v-html="`${char.value} : ${char.score}`" :id="char.id" class="stack__char"
+          :key="char.id" :ref="char.id" draggable="true" @dragstart="dragChar"></div>
       </div>
     </div>
   </div>
-
-  <div v-if="isGameEnded">
+  <div v-if="hasGameEnded">
         <!-- EndPage -->
         <p class="winnerMessage">
             Congrats {{ winner }}, you won! You finished the game in {{ timeCompleted }} minute(s) and with {{ winnerPoints }} points!
@@ -344,121 +109,160 @@ export default {
       durationInSeconds: 180,
       timeRemaining: 0,
       timerInterval: null,
-      isGameEnded: false,
+      hasGameEnded: false,
       chars: [
         {
+          id: 1,
           value: 'e',
           score: 3,
         },
         {
+          id: 2,
           value: 'a',
           score: 3,
         },
         {
+          id: 3,
           value: 'i',
           score: 3,
         },
         {
+          id: 4,
           value: 'o',
           score: 3,
         },
         {
+          id: 5,
           value: 'n',
           score: 3,
         },
         {
+          id: 6,
           value: 'r',
           score: 3,
         },
         {
+          id: 7,
           value: 't',
           score: 3,
         },
         {
+          id: 8,
           value: 'l',
           score: 3,
         },
         {
+          id: 9,
           value: 's',
           score: 3,
         },
         {
+          id: 10,
           value: 'u',
           score: 3,
         },
         {
-          value: '',
+          id: 11,
+          value: 'space',
           score: 0,
         },
         {
+          id: 11,
+          value: 'space',
+          score: 0,
+        },
+        {
+          id: 12,
           value: 'd',
           score: 6,
         },
         {
+          id: 13,
           value: 'g',
           score: 6,
         },
         {
+          id: 14,
           value: 'b',
           score: 9,
         },
         {
+          id: 15,
           value: 'c',
           score: 9,
         },
         {
+          id: 16,
           value: 'm',
           score: 9,
         },
         {
+          id: 17,
           value: 'p',
           score: 9,
         },
         {
+          id: 18,
           value: 'f',
           score: 12,
         },
         {
+          id: 19,
           value: 'h',
           score: 12,
         },
         {
+          id: 20,
           value: 'v',
           score: 12,
         },
         {
+          id: 21,
           value: 'w',
           score: 12,
         },
         {
+          id: 22,
           value: 'y',
           score: 12,
         },
         {
+          id: 23,
           value: 'k',
           score: 15,
         },
         {
+          id: 24,
           value: 'j',
           score: 24,
         },
         {
+          id: 25,
           value: 'x',
           score: 24,
         },
         {
+          id: 26,
           value: 'q',
           score: 30,
         },
         {
+          id: 27,
           value: 'z',
           score: 30,
         },
         {
+          id: 28,
+          value: "'",
+          score: 0,
+        },
+        {
+          id: 29,
           value: "'",
           score: 0,
         }
       ],
+      stack: null,
       winner: null,
       timeCompleted: null,
       winnerPoints: null,
@@ -477,6 +281,7 @@ export default {
   mounted() {
     this.timeRemaining = this.durationInSeconds;
     this.timerInterval = setInterval(this.updateTimer, 1000);
+    this.stack = this.generateStack();
   },
 
   methods: {
@@ -489,9 +294,13 @@ export default {
     endGame() {
       clearInterval(this.timerInterval);
     },
-    generateChars() {
-      // 
-      return [{ id: 1, value: 'a', score: 4 }, { id: 2, value: 'b', score: 5 }]
+    generateStack() {
+      // Generates a stack of random letter-objects
+      const stack = [];
+      for (let i = 0; i < 20; i++) {
+        stack.push(this.chars[Math.floor(Math.random() * this.chars.length)]);
+      }
+      return stack;
     },
     dragChar(e) {
       e.dataTransfer.setData('text/plain', e.target.id);
@@ -501,8 +310,6 @@ export default {
       const charId = e.dataTransfer.getData('text/plain');
       const draggedChar = document.getElementById(charId);
       e.target.innerHTML = draggedChar.innerHTML;
-
-      
     },
   }
 };
@@ -523,14 +330,6 @@ export default {
 }
 
 /* Deck */
-.main__decks {
-  display: flex;
-}
-
-.decks__deck {
-  width: 25%;
-}
-
 .deck__rack {
   margin: 15px 0px;
 }
